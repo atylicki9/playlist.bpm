9808219a8e14a5a5265bbcce0de00229bc4ea6e8
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-10-24 14:37:57 -0400

    update pace input

--- src/components/paceMaxInput/index.tsx
+++ src/components/paceMaxInput/index.tsx
@@ -1,21 +1,28 @@
 import {
     Box,
-    Input
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper
   } from "@chakra-ui/react";
   import React from 'react';
   
   interface PaceMaxInputProps{
     exercise: string;
-
   }
   
   export const PaceMaxInput: React.FC<PaceMaxInputProps> = ({exercise}) => {
     return (
       <Box display={"flex"} alignItems="center">
         <h2>{exercise}</h2>
-        <Input
-          type={""}
-        />
+        <NumberInput step={.25} defaultValue={10} min={0} max={20} paddingLeft={5}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
       </Box>
     );
   };
