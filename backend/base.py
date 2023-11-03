b11f79cfba5ac462ae3456f1c91c845096be9e7c
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-11-03 16:57:18 -0400

    ping endpoint

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
+# to run>
+# in backend directory: source env/bin/activate
+# then enter flask run and navigate to api 
+
 api = Flask(__name__)
 
-@api.route('/profile')
+@api.route('/ping')
 def my_profile():
     response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        "about" :"Workout Generator API is Online."
     }
 
     return response_body
