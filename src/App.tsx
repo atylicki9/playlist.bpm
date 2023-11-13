7ef985d19d3770fcc4d1ed24a72899f4b94a3cc2
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-11-12 21:11:50 -0500

    attempt at setting state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -13,6 +13,12 @@ import { InfoBox } from './components/infoBox';
 import { DisclaimerBox } from './components/disclaimerBox';
 import { SubmitButton } from './components/submitButton';
 
+const [pushups, setPushups] = useState(0);
+const [situps, setSitups] = useState(0);
+const [pullups, setPullups] = useState(0);
+const [run, setRun] = useState(0);
+const [swim, setSwim] = useState(0);
+
 export const App: React.FC = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
@@ -20,9 +26,9 @@ export const App: React.FC = () => (
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <InfoBox/>
-          <ExerciseMaxInput exercise={"Pushups"} maxValue={100} />
-          <ExerciseMaxInput exercise={"Situps"} maxValue={100} />
-          <ExerciseMaxInput exercise={"Pullups"} maxValue={100} />
+          <ExerciseMaxInput exercise={"Pushups"} maxValue={100} value={pushups}/>
+          <ExerciseMaxInput exercise={"Situps"} maxValue={100} value={situps}/>
+          <ExerciseMaxInput exercise={"Pullups"} maxValue={100} value={pullups}/>
           <PaceMaxInput exercise={"1.5 Mile Run"} />
           <PaceMaxInput exercise={"500 Yard Swim"} />
           <SubmitButton/>
--- src/components/exerciseMaxInput/index.tsx
+++ src/components/exerciseMaxInput/index.tsx
@@ -9,13 +9,20 @@ import React from 'react';
 interface ExerciseMaxInputProps{
   exercise: string;
   maxValue: number;
+  value: number;
 }
 
-export const ExerciseMaxInput: React.FC<ExerciseMaxInputProps> = ({exercise, maxValue}) => {
+export const ExerciseMaxInput: React.FC<ExerciseMaxInputProps> = ({exercise, maxValue, value}) => {
   return (
     <Box display={"flex"} alignItems="center">
       <h2>{exercise}</h2>
-      <NumberInput step={1} defaultValue={0} min={0} max={maxValue} paddingLeft={5}>
+      <NumberInput 
+        step={1} 
+        defaultValue={0} 
+        min={0} 
+        max={maxValue} 
+        paddingLeft={5}
+        value={value}>
         <NumberInputField />
         <NumberInputStepper>
           <NumberIncrementStepper />
