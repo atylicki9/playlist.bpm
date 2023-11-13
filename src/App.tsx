a09274b90770ffaf35c9ac091a7311c8dcb12ede
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-11-12 21:37:02 -0500

    reformat app class

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,30 @@ import { InfoBox } from './components/infoBox';
 import { DisclaimerBox } from './components/disclaimerBox';
 import { SubmitButton } from './components/submitButton';
 
-export const App: React.FC = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <InfoBox/>
-          <ExerciseMaxInput exercise={"Pushups"} maxValue={100} />
-          <ExerciseMaxInput exercise={"Situps"} maxValue={100} />
-          <ExerciseMaxInput exercise={"Pullups"} maxValue={100} />
-          <PaceMaxInput exercise={"1.5 Mile Run"} />
-          <PaceMaxInput exercise={"500 Yard Swim"} />
-          <SubmitButton/>
-        </VStack>
-        <DisclaimerBox/>  
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App: React.FC = () => {
+
+
+
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <InfoBox/>
+            <ExerciseMaxInput exercise={"Pushups"} maxValue={100} />
+            <ExerciseMaxInput exercise={"Situps"} maxValue={100} />
+            <ExerciseMaxInput exercise={"Pullups"} maxValue={100} />
+            <PaceMaxInput exercise={"1.5 Mile Run"} />
+            <PaceMaxInput exercise={"500 Yard Swim"} />
+            <SubmitButton/>
+          </VStack>
+          <DisclaimerBox/>  
+        </Grid>
+      </Box>
+    </ChakraProvider>
+
+
+  )
+}
