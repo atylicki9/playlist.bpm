2fe0ba3ae39b2e03bac85c4da463d388eef10900
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-12-03 20:22:41 -0500

    add excel utility

---
+++ src/utility/excelUtility.ts
@@ -0,0 +1,24 @@
+import ExcelJS from 'exceljs';
+
+export async function selectExcelFile(filePath:string, data: string) {
+    // Open the existing Excel file
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+
+    return workbook;
+}
+
+export async function saveExcelFile(workbook: ExcelJS.Workbook, filePath:string) {
+    // Save the changes to the file
+    await workbook.xlsx.writeFile(filePath);
+}
+
+export function updateExcelCell(workbook: ExcelJS.Workbook, data: string, cell: string, sheet: number = 1) {
+
+    const currentSheet = workbook.getWorksheet(sheet);
+    // Update the data in the sheet
+    if (currentSheet)
+    {
+        currentSheet.getCell(cell).value = data;
+    }
+}
