de5dcea536f6076b5cac0f40f78373f87b33da7d
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2024-02-08 08:37:08 -0500

    remove some files

--- src/components/disclaimerBox/index.tsx
+++ src/components/disclaimerBox/index.tsx
@@ -4,14 +4,7 @@ import {
 } from "@chakra-ui/react";
 import React from 'react';
 
-const disclaimer:string = "DISCLAIMER: Preparation for this training can be equally strenuous. " +
-"You should consult a physician before you begin any strenuous exercise program, such as the one described here, " +
-"or any diet modification, especially if you have or suspect that you have high blood pressure, diabetes, " +
-"or any other adverse medical conditions. If you feel faint or dizzy at any time while performing any portion " +
-"of this training program, stop immediately and seek medical evaluation. " +
-"The United States Government and any service member, the creator of this website, or civilian employed by the United States Government disclaims " + 
-"any liability, personal or professional, resulting from the misapplication of any training procedure, technique, or " +
-"guidance described in this guide."
+const disclaimer:string = "DISCLAIMER: Figure out if i need one of these....."
 
 export const DisclaimerBox = () => {
   return (
