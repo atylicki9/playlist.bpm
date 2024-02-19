d5923895c15a6e90725d04fc0c16cccd64c25835
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2024-02-19 08:06:48 -0500

    remove comments

--- backend/base.py
+++ backend/base.py
@@ -12,12 +12,6 @@ api = Flask(__name__)
 @api.route('/playlists', methods = ['POST'])
 @cross_origin(origin='*')
 def playlists():
-    """
-    Creates a new playlist with the provided information.
-
-    Returns:
-        dict: The playlist information.
-    """
     headers = {
         'Authorization': 'Bearer '+ request.json['token'],
         'Content-Type': 'application/json'
@@ -31,7 +25,6 @@ def playlists():
 
     response = requests.get('https://api.spotify.com/v1/recommendations',  params=playlistInfo, headers=headers )
 
-        # Check if the request was successful
     if response.status_code == 200:
         recommendations = response.json()
         print(recommendations)        
