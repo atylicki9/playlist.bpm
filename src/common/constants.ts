237b010b3580e2fdeaaf906c81abe1b0af53f92f
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2024-02-21 20:04:32 -0500

    add genres

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -2,4 +2,4 @@
 export const API_PLAYLISTS_ENDPOINT: string = 'http://127.0.0.1:5000/playlists'
 
 // Genres
-export const possibleGenres: string[] = ['Rock', 'Pop', 'Latin', 'Jazz', 'EDM', 'Metal', 'Classical'];
+export const possibleGenres: string[] = ['Rock', 'Country','Hip-Hop', 'Pop', 'Latin', 'Jazz', 'EDM', 'Metal', 'Classical'];
